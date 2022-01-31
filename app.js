b9a015a72d4869805d7bdea432b9d55a62fc68d0
mjc14872 <14872@uagro.mx>
2022-01-31 09:55:31 -0600

    agregando paquetes

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.use(express.static(publicPath)) //ruta de archivos públicos
 
 //app.use('/static', express.static(__dirname + '/public'))
 
-app.listen(3030, () => {
-    console.log('Servidor corriendo en el puerto 3030');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
 });
 
 app.get('/', (req, res) => {
