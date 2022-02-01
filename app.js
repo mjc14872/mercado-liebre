f3588a63780a81b7126c7cf3b72cf8a67ac1442d
mjc14872 <14872@uagro.mx>
2022-01-31 20:46:03 -0600

    probando algo

--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath)) //ruta de archivos públicos
-
-
-//app.use('/static', express.static(__dirname + '/public'))
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
