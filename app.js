b897982b16ea9ba0d51400b205e1582eed3bbbae
mjc14872 <14872@uagro.mx>
2022-02-16 07:11:46 -0600

    completo con algunos detalles

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
+app.listen(process.env.PORT || 3040, () => {
+    console.log('Servidor corriendo en el puerto 3040');
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'))
-})
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'))
+});
+
+app.get('/registro', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/registro.html'))
+});
+
